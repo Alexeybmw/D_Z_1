8fd4aa348ca7122231f3bfb6317cc92ad6aebc04
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-07 20:53:39 +0300

    определение четного и не четного числа

---
+++ zadacha_6/Program.cs
@@ -0,0 +1,9 @@
+﻿double number = 7;
+double result = number;
+
+     if (number % 2 == 0) 
+          Console.WriteLine(" even ");
+      
+     if( number % 2 > 0)      
+          Console.WriteLine(" odd  "); 
+          
