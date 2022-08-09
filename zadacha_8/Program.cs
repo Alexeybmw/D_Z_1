8004941c67eeaafc19a945a457dcd00f83284ba7
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-09 09:17:48 +0300

    нашли четные числа от 1 до n

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = 8;
+int i = 1;
+
+Console.WriteLine("n -> " +n);
+while (i<=n)
+    {
+    if( i % 2 == 0)
+    
+        Console.Write(i+";");
+        i=++i;
+    }
+    
+    
+    
+    
+    
+
+            
+            
+    
+    
+
